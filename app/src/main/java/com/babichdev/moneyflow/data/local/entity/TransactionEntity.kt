126815566d3f085b3f34844ec807684bab9d2438
Aleksandr Petrovich <alexander85b@gmail.com>
2026-07-22 02:23:01 +0700

    Configure Room and KSP on AGP 9.3

---
+++ app/src/main/java/com/babichdev/moneyflow/data/local/entity/TransactionEntity.kt
@@ -0,0 +1,17 @@
+package com.babichdev.moneyflow.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "transactions")
+data class TransactionEntity(
+
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+
+    val title: String,
+
+    val amount: Double,
+
+    val isIncome: Boolean
+)
